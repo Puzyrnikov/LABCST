91c3b70533d0c12120bbda8896f72ea532cb9146
Сергей <@WIND>
2016-06-28 23:11:42 +0500

    first

---
+++ LabCST/LabCST.WebUI/Controllers/Page4Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabCST.WebUI.Controllers
+{
+    public class Page4Controller : Controller
+    {
+        // GET: Page4
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LabCST/LabCST.WebUI/Controllers/Page5Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabCST.WebUI.Controllers
+{
+    public class Page5Controller : Controller
+    {
+        // GET: Page5
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
